90f18f3bc6a2fcad61d7dd7acc512c1bf4612831
untuned1337 <52298228+untuned1337@users.noreply.github.com>
2020-02-06 17:55:12 +0200

    Create main.py

---
+++ IP_LAB1/main.py
@@ -0,0 +1,26 @@
+class Data(object) :
+    def __init__(self, id, hours, income, age, education, child5, child13, child17, nonwhite, owned, mortgage, occupation, unemp):
+        self.id = id
+        self.hours = hours
+        self.income = income
+        self.age = age
+        self.education = education
+        self.child5 = child5
+        self.child13 = child13
+        self.child17 = child17
+        self.nonwhite = nonwhite
+        self.owned = owned
+        self.mortgage = mortgage
+        self.occupation = occupation
+        self.unemp = unemp
+
+
+
+
+def Min(dataArr):
+    min = dataArr[0]
+    for data in dataArr:
+        print(5);
+
+
+
